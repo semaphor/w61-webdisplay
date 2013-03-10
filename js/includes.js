1b8cfb0a5cbe7b75c05d0e0ca3f393370343550c
Simon Lüke <semaphor@resolution1.net>
2013-03-10 12:42:20 +0100

    added further tabs; niced layout.

--- js/includes.js
+++ js/includes.js
@@ -7,11 +7,11 @@ function include(what)
 			<header id="topbar" class="body">\
 				<nav id="menu">\
 					<ul>\
-						<li><a href="index.html">START</a></li>\
+						<li><a href="index.html">HOME</a></li>\
 						<li><a href="map.html">Karte</a></li>\
-						<li>blubber</li>\
-						<li>blabber</li>\
-						<li><a href="info.html">Infos/Hilfe</a></li>\
+						<li><a href="accounting.html">Einkaufsliste</a></li>\
+						<li><a href="documentation.html">Infos</a></li>\
+						<li><a href="info.html">Über</a></li>\
 					</ul>\
 				</nav>\
 				<div id="indicator">\
@@ -23,7 +23,7 @@ function include(what)
 	case 'bottomline':
 		document.write('\
 			<footer id="bottomline" class="body">\
-				<span id="info" align="center"><small><em>Aktuell ist dies eher eine Skizze. Zwar funktionieren die eingebundenen Bus- und Müllabfuhrpläne oder die Karte schon, aber eigentlich alles ist noch feinste <strong>work in progress</strong>.</em></small></span>\
+				<span class="footnote" align="center"><small><em>Aktuell ist dies eher eine Skizze. Zwar funktionieren die eingebundenen Bus- und Müllabfuhrpläne oder die Karte schon, aber eigentlich alles ist noch feinste <strong><a href="info.html">work in progress</a></strong>.</em></small></span>\
 			</footer>\
 		');
 		break;
